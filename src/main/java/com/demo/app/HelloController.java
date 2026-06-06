bfcade67b1b44e3e362aa8f396cb3ffaa7fe4841
Saumya Singh <saumyasingh7379@gmail.com>
2026-06-06 10:27:19 +0530

    Initial WAR project

---
+++ src/main/java/com/demo/app/HelloController.java
@@ -0,0 +1,13 @@
+package com.demo.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "WAR Deployment Successful from Jenkins!";
+    }
+}
